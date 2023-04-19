5e30411556f644ab17a351cce204c96acafd8d25
Mateusz Furga <matfurga@gmail.com>
2023-04-19 14:21:00 +0200

    Add reset

--- cc1101.cc
+++ cc1101.cc
@@ -1,4 +1,85 @@
-#include <SPI.h>
-
 #include "cc1101.h"
+
+Status CC1101::begin() {
+  pinMode(cs, OUTPUT);
+  chipDeselect();
+
+  SPI.begin();
+
+  hardReset();
+  delay(2);
+
+  byte partnum = getChipPartNumber();
+  byte version = getChipVersion();
+
+  if (partnum != CC1101_PARTNUM || version != CC1101_VERSION) {
+    return STATUS_ERROR_CHIP_NOT_FOUND;
+  }
+
+  return STATUS_OK;
+}
+
+byte CC1101::readReg(byte addr) {
+  byte header = CC1101_READ | (addr & 0b111111);
+
+  if (addr >= 0x30) {
+    /* Status registers - access with the burst bit on. */
+    header |= CC1101_BURST;
+  }
+
+  SPI.beginTransaction(spiSettings);
+
+  chipSelect();
+  waitReady();
+
+  SPI.transfer(header);
+  byte result = SPI.transfer(0x00);
+
+  chipDeselect();
+
+  SPI.endTransaction();
+  return result;
+}
+
+void CC1101::hardReset() {
+  SPI.beginTransaction(spiSettings);
+
+  chipDeselect();
+  delayMicroseconds(5);
+  chipSelect();
+  delayMicroseconds(5);
+  chipDeselect();
+  delayMicroseconds(40);
+
+  chipSelect();
+  waitReady();
+
+  SPI.transfer(CC1101_SRES);
+
+  waitReady();
+  chipDeselect();
+
+  SPI.endTransaction();
+}
+
+void CC1101::chipSelect() {
+  digitalWrite(cs, LOW);
+}
+
+void CC1101::chipDeselect() {
+  digitalWrite(cs, HIGH);
+}
+
+void CC1101::waitReady() {
+  while (digitalRead(MISO))
+    ;
+}
+
+byte CC1101::getChipPartNumber() {
+  return readReg(CC1101_REG_PARTNUM);
+}
+
+byte CC1101::getChipVersion() {
+  return readReg(CC1101_REG_VERSION);
+}
 
--- cc1101.h
+++ cc1101.h
@@ -1,16 +1,60 @@
 #pragma once
 
 #include <Arduino.h>
+#include <SPI.h>
 
 #define PIN_UNUSED 0xff
 
-class CC1101 {
- public:
-  CC1101(byte ss, byte gd0 = PIN_UNUSED, byte gd2 = PIN_UNUSED)
-    : ss(ss), gd0(gd0), gd2(gd2) {}
+#define CC1101_SPI_MAX_FREQ    6500000    /* 6.5 MHz */
+#define CC1101_SPI_DATA_ORDER  MSBFIRST
+#define CC1101_SPI_DATA_MODE   SPI_MODE0  /* clk low, leading edge */
 
- private:
-  byte ss, gd0, gd2;
+#define CC1101_WRITE           0x00
+#define CC1101_READ            0x80
+#define CC1101_BURST           0x40
+
+#define CC1101_PARTNUM         0x00
+#define CC1101_VERSION         0x14
+
+/* Command strobes */
+#define CC1101_SRES            0x30
+
+/* Registers */
+
+/* Status registers */
+#define CC1101_REG_PARTNUM     0x30
+#define CC1101_REG_VERSION     0x31
+
+enum Status {
+  STATUS_OK = 0,
+
+  STATUS_ERROR_CHIP_NOT_FOUND
 };
 
+class CC1101 {
+ public:
+  CC1101(byte cs, byte gd0 = PIN_UNUSED, byte gd2 = PIN_UNUSED)
+    : cs(cs),
+      gd0(gd0),
+      gd2(gd2),
+      spiSettings(CC1101_SPI_MAX_FREQ,
+                  CC1101_SPI_DATA_ORDER,
+                  CC1101_SPI_DATA_MODE) {}
+  Status begin();
+
+  byte getChipPartNumber();
+  byte getChipVersion();
+
+  byte readReg(byte addr);
+
+ private:
+  void chipSelect();
+  void waitReady();
+  void chipDeselect();
+
+  void hardReset();
+
+  byte cs, gd0, gd2;
+  SPISettings spiSettings;
+};
 
