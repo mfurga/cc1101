8661470302bdf1403405bdf4e28deba99309bf29
Mateusz Furga <matfurga@gmail.com>
2023-04-20 19:15:47 +0200

    Update

--- cc1101.cc
+++ cc1101.cc
@@ -7,7 +7,7 @@ Status CC1101::begin() {
   SPI.begin();
 
   hardReset();
-  delay(2);
+  delay(10);
 
   byte partnum = getChipPartNumber();
   byte version = getChipVersion();
@@ -16,30 +16,76 @@ Status CC1101::begin() {
     return STATUS_ERROR_CHIP_NOT_FOUND;
   }
 
+  setRegs();
+
+  debug();
+
+  sendCmd(CC1101_CMD_IDLE);
+  flushRxBuffer();
+  flushTxBuffer();
+
   return STATUS_OK;
 }
 
-byte CC1101::readReg(byte addr) {
-  byte header = CC1101_READ | (addr & 0b111111);
-
-  if (addr >= 0x30) {
-    /* Status registers - access with the burst bit on. */
-    header |= CC1101_BURST;
-  }
-
-  SPI.beginTransaction(spiSettings);
-
-  chipSelect();
-  waitReady();
-
-  SPI.transfer(header);
-  byte result = SPI.transfer(0x00);
-
-  chipDeselect();
-
-  SPI.endTransaction();
-  return result;
+void CC1101::setRegs() {
+  writeReg(CC1101_REG_MCSM0, CC1101_DEFVAL_MCSM0);
+  writeReg(CC1101_REG_PKTCTRL0, CC1101_DEFVAL_PKTCTRL0);
 }
+
+void CC1101::setFrequency(double frequency) {
+  uint32_t freq = (uint32_t)((frequency * 65536.0) / 26.0);
+
+  sendCmd(CC1101_CMD_IDLE);
+
+  writeReg(CC1101_REG_FREQ0, freq & 0xff);
+  writeReg(CC1101_REG_FREQ1, (freq >> 8) & 0xff);
+  writeReg(CC1101_REG_FREQ2, (freq >> 16) & 0xff);
+}
+
+void CC1101::transmit(byte data) {
+  byte txFifoBytes;
+
+  sendCmd(CC1101_CMD_IDLE);
+  flushTxBuffer();
+
+  byte paValues[2] = { 0x00, 0xc0 };
+  writeRegBurst(CC1101_REG_PATABLE, paValues, 2);
+
+  writeReg(CC1101_REG_FREND0, 0b00010001);
+
+  writeReg(CC1101_REG_MDMCFG2, CC1101_DEFVAL_MDMCFG2);
+
+  txFifoBytes = readReg(CC1101_REG_TXBYTES) & ~(1 << 7);
+  Serial.printf("Bytes in TX FIFO: %d\r\n", txFifoBytes);
+
+  writeReg(CC1101_REG_FIFO, data);
+  sendCmd(CC1101_CMD_TX);
+
+  do {
+    txFifoBytes = readReg(CC1101_REG_TXBYTES) & ~(1 << 7);
+  } while (txFifoBytes > 0);
+
+}
+
+void CC1101::saveStatus(byte status) {
+  currentState = (CC1101_State)((status >> 3) & 0b111);
+}
+
+void CC1101::debug() {
+  Serial.printf(
+    "=== CC1101 DEBUG:\r\n"
+    "  state: %x\r\n",
+    currentState
+  );
+
+  Serial.println();
+}
+
+/*
+byte CC1101::getStatusByte() {
+  return sendCmd(CC1101_CMD_NOP);
+}
+*/
 
 void CC1101::hardReset() {
   SPI.beginTransaction(spiSettings);
@@ -54,11 +100,102 @@ void CC1101::hardReset() {
   chipSelect();
   waitReady();
 
-  SPI.transfer(CC1101_SRES);
+  SPI.transfer(CC1101_CMD_RES);
 
   waitReady();
   chipDeselect();
 
+  SPI.endTransaction();
+}
+
+void CC1101::flushRxBuffer() {
+  if (currentState != STATE_IDLE && currentState != STATE_RXFIFO_OVERFLOW) {
+    return;
+  }
+  sendCmd(CC1101_CMD_FRX);
+}
+
+void CC1101::flushTxBuffer() {
+  if (currentState != STATE_IDLE && currentState != STATE_TXFIFO_UNDERFLOW) {
+    return;
+  }
+  sendCmd(CC1101_CMD_FTX);
+}
+
+byte CC1101::readReg(byte addr) {
+  byte header = CC1101_READ | (addr & 0b111111);
+
+  if (addr >= CC1101_REG_PARTNUM && addr <= CC1101_REG_RCCTRL0_STATUS) {
+    /* Status registers - access with the burst bit on. */
+    header |= CC1101_BURST;
+  }
+
+  SPI.beginTransaction(spiSettings);
+  chipSelect();
+  waitReady();
+
+  saveStatus(SPI.transfer(header));
+  byte data = SPI.transfer(0x00);
+
+  //Serial.printf("DEBUG: Reading reg %02x = %02x\r\n", addr, data);
+
+  chipDeselect();
+  SPI.endTransaction();
+  return data;
+}
+
+void CC1101::writeReg(byte addr, byte data) {
+  if (addr >= CC1101_REG_PARTNUM && addr <= CC1101_REG_RCCTRL0_STATUS) {
+    /* Status registers are read-only. */
+    return;
+  }
+
+  byte header = CC1101_WRITE | (addr & 0b111111);
+
+  SPI.beginTransaction(spiSettings);
+  chipSelect();
+  waitReady();
+
+  saveStatus(SPI.transfer(header));
+  saveStatus(SPI.transfer(data));
+
+  chipDeselect();
+  SPI.endTransaction();
+}
+
+void CC1101::writeRegBurst(byte addr, byte *data, size_t size) {
+  if (addr >= CC1101_REG_PARTNUM && addr <= CC1101_REG_RCCTRL0_STATUS) {
+    /* Status registers are read-only. */
+    return;
+  }
+
+  byte header = CC1101_WRITE | CC1101_BURST | (addr & 0b111111);
+
+  SPI.beginTransaction(spiSettings);
+  chipSelect();
+  waitReady();
+
+  saveStatus(SPI.transfer(header));
+  for (size_t i = 0; i < size; i++) {
+    saveStatus(SPI.transfer(data[i]));
+  }
+
+  chipDeselect();
+  SPI.endTransaction();
+}
+
+void CC1101::sendCmd(byte addr) {
+  byte header = CC1101_WRITE | (addr & 0b111111);
+
+  Serial.printf("DEBUG: Command %02x\r\n", addr);
+
+  SPI.beginTransaction(spiSettings);
+  chipSelect();
+  waitReady();
+
+  saveStatus(SPI.transfer(header));
+
+  chipDeselect();
   SPI.endTransaction();
 }
 
--- cc1101.h
+++ cc1101.h
@@ -17,18 +17,73 @@
 #define CC1101_VERSION         0x14
 
 /* Command strobes */
-#define CC1101_SRES            0x30
+#define CC1101_CMD_RES         0x30  /* Reset chip */
+#define CC1101_CMD_RX          0x34  /* Enable RX */
+#define CC1101_CMD_TX          0x35  /* Enable TX */
+#define CC1101_CMD_IDLE        0x36  /* Enable IDLE */
+#define CC1101_CMD_FRX         0x3a  /* Flush the RX FIFO buffer */
+#define CC1101_CMD_FTX         0x3b  /* Flush the TX FIFO buffer */
+#define CC1101_CMD_NOP         0x3d  /* No operation */
 
 /* Registers */
+#define CC1101_REG_PKTCTRL0    0x08  /* Packet Automation Control */
+
+#define CC1101_REG_FREQ2       0x0d
+#define CC1101_REG_FREQ1       0x0e
+#define CC1101_REG_MDMCFG2     0x12  /* Modem Configuration */
+#define CC1101_REG_FREQ0       0x0f
+
+#define CC1101_REG_MCSM2       0x16
+#define CC1101_REG_MCSM1       0x17
+#define CC1101_REG_MCSM0       0x18
+#define CC1101_REG_FREND0      0x22  /* Front End TX Configuration */
+
+#define CC1101_REG_PATABLE     0x3e
+#define CC1101_REG_FIFO        0x3f
 
 /* Status registers */
-#define CC1101_REG_PARTNUM     0x30
-#define CC1101_REG_VERSION     0x31
+#define CC1101_REG_PARTNUM        0x30
+#define CC1101_REG_VERSION        0x31
+#define CC1101_REG_TXBYTES        0x3a
+#define CC1101_REG_RXBYTES        0x3b
+#define CC1101_REG_RCCTRL0_STATUS 0x3d
+
+/* Default registers values */
+
+/*
+  7:6 - Not used.
+  5:4 - FS_AUTOCAL[1:0] - Automatically calibrate when going to RX or TX
+                          or back to IDLE
+    00 - Never (manually calibrate using SCAL strobe)
+    01 - When going from IDLE to RX or TX (or FSTXON)
+    10 - When going from RX or TX back to IDLE automatically
+    11 - Every 4th time when going from RX or TX to IDLE automatically
+*/
+#define CC1101_DEFVAL_MCSM0    0b00010100
+
+/* Disable CRC, fixed packet length mode */
+#define CC1101_DEFVAL_PKTCTRL0 0b00000000
+
+#define CC1101_DEFVAL_MDMCFG2  0b00110000
 
 enum Status {
   STATUS_OK = 0,
 
+  STATUS_INVALID_PARAM,
   STATUS_ERROR_CHIP_NOT_FOUND
+};
+
+enum CC1101_State {
+  STATE_IDLE = 0,         /* IDLE state */
+  STATE_RX,               /* Receive mode */
+  STATE_TX,               /* Transmit mode */
+  STATE_FSTXON,           /* Fast TX ready */
+  STATE_CALIBRATE,        /* Frequency synthesizer calibration is running */
+  STATE_SETTLING,         /* PLL is settling */
+  STATE_RXFIFO_OVERFLOW,  /* RX FIFO has overflowed */
+  STATE_TXFIFO_UNDERFLOW, /* TX FIFO has underflowed */
+
+  STATE_UNKNOWN           /* Unknown state */
 };
 
 class CC1101 {
@@ -45,16 +100,32 @@ class CC1101 {
   byte getChipPartNumber();
   byte getChipVersion();
 
-  byte readReg(byte addr);
+  void setFrequency(double freq);
+
+  void transmit(byte data);
+  void debug();
 
  private:
   void chipSelect();
   void waitReady();
   void chipDeselect();
 
+  byte readReg(byte addr);
+  void sendCmd(byte addr);
+
+  void writeReg(byte addr, byte data);
+  void writeRegBurst(byte addr, byte *data, size_t size);
+
+  void setRegs();
   void hardReset();
+  void flushRxBuffer();
+  void flushTxBuffer();
+
+  void saveStatus(byte status);
 
   byte cs, gd0, gd2;
   SPISettings spiSettings;
+
+  CC1101_State currentState = STATE_UNKNOWN;
 };
 
